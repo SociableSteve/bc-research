6c9271e46810baacc66c32b4c6fd2a2f9a1e5ea6
Steve Goode <steve.goode@nearform.com>
2023-05-13 17:46:24 +0100

    Set bc-research subdomain for locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,10 @@
 import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next'; import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
 
 i18next.use(initReactI18next).use(Backend).init({
-    fallbackLng: 'en'
+    fallbackLng: 'en',
+    backend: {
+        loadPath: '/bc-research/locales/{{lng}}/{{ns}}.json',
+    }
 })
