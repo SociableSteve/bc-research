109fd9fee713d007d678f0da913d5f3c0db2bedb
Steve Goode <steve.goode@nearform.com>
2023-05-13 18:00:55 +0100

    Added browser language detection

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,9 @@
 import i18next from 'i18next';
+import languageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 
-i18next.use(initReactI18next).use(Backend).init({
+i18next.use(initReactI18next).use(languageDetector).use(Backend).init({
     fallbackLng: 'en',
     backend: {
         loadPath: '/bc-research/locales/{{lng}}/{{ns}}.json',
